29bc8791cc6d72cbe57be0847f446c96842874dc
lorislorenzo <derek4783@hotmail.fr>
2026-05-08 22:41:54 +1100

    ajout more urls à scan

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ import re
 import os
 from dotenv import load_dotenv
 
+#fonction qui vérifie les matchs
+def annonce_match_keywords(titre, keywords):
+    for mot in keywords:
+        mot = normalize(mot)
+        if re.search(rf"\b{re.escape(mot)}\b", titre):
+            return True
+    return False
 #fonction pour gérer les accents
 def normalize(text):
     text = text.lower()
@@ -15,7 +22,20 @@ def normalize(text):
         if unicodedata.category(c) != 'Mn'
     )
 # url de l'api annonces.nc"
-URL = "https://api.annonces.nc/posts?sort=-created_at&per=30&by_site=annonces.nc&by_locality=nouvelle-caledonie" 
+AlwaysAlertURL = "https://api.annonces.nc/posts?by_category=jeux-consoles-1&per=40&sort=-published_at&by_locality=nouvelle-caledonie&page=1"
+URL = [
+    "https://api.annonces.nc/posts?sort=-created_at&per=30&by_site=annonces.nc&by_locality=nouvelle-caledonie",
+    "https://api.annonces.nc/posts?by_category=tv-video-1&per=40&sort=-published_at&by_locality=nouvelle-caledonie&page=1",
+    "https://api.annonces.nc/posts?by_category=informatique-1&per=40&sort=-published_at&by_locality=nouvelle-caledonie&page=1",
+    "https://api.annonces.nc/posts?by_category=jeux-societe-1&per=40&sort=-published_at&by_locality=nouvelle-caledonie&page=1",
+    "https://api.annonces.nc/posts?by_category=jouets-1&per=40&sort=-published_at&by_locality=nouvelle-caledonie&page=1",
+    "https://api.annonces.nc/posts?by_category=livres-revues-bd-1&per=40&sort=-published_at&by_locality=nouvelle-caledonie&page=1",
+    "https://api.annonces.nc/posts?by_category=collections-1&per=40&sort=-published_at&by_locality=nouvelle-caledonie&page=1",
+    "https://api.annonces.nc/posts?by_category=divers-1&per=40&sort=-published_at&by_locality=nouvelle-caledonie&page=1",
+    AlwaysAlertURL
+]
+# URL sur lequel je récupère l'intégralité des annonces
+
 # id de mon token telegram
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -37,23 +57,22 @@ except:  # noqa: E722
 
 while True:
     new_ids = set()
-    # ance la requête pour récuper les annonces
-    response = requests.get(URL)
-    # on stock les données dans une variable
-    data = response.json()
-    # parcourir les annonces et stock les id et les titres
-    for annonce in data:
-        id_annonce = annonce["id"]
-        titre_annonce = normalize(annonce["title"])
-        price_annonce = annonce["price"]
-        # on vérifie si l'id n'est pas dans le json des id
-        if id_annonce not in seen_ids:
-        # boucle pour vérifier si un mot clé est dans le titre de l'annonce
-            for mot in KEYWORDS:
-                mot = normalize(mot)
-                if re.search(rf"\b{re.escape(mot)}\b", titre_annonce):
+    # lance la requête pour récuper les annonces
+    for url in URL :
+        response = requests.get(url)
+        # on stock les données dans une variable
+        data = response.json()
+        # parcourir les annonces et stock les id et les titres
+        for annonce in data:
+            id_annonce = annonce["id"]
+            titre_annonce = normalize(annonce["title"])
+            price_annonce = annonce["price"]
+            # on vérifie si l'id n'est pas dans le json des id
+            if id_annonce not in seen_ids:
+            # boucle pour vérifier si un mot clé est dans le titre de l'annonce
+                if annonce_match_keywords(titre_annonce, KEYWORDS) or url == AlwaysAlertURL:
                     print(f"Nouveau match : {titre_annonce}")
-                # variable du message qui sera envoyé sur telegram
+                    # variable du message qui sera envoyé sur telegram
                     message = f"""
                     🔥 Nouveau match !
                     Titre : {titre_annonce}
@@ -67,20 +86,15 @@ while True:
                         "parse_mode": "HTML"
                     }
                     # envoie le message via le bot telegram
-                    response_telegram = requests.post(url_telegram, data=payload)
-                    print(response_telegram.status_code)
-                    print(response_telegram.text)
-                    # break pour éviter d'envoyer plusieurs messages pour la même annonce si elle contient plusieurs mots clés
-                    break
-            # si l'id n'est pas dans le json on le stock
-            
-            if id_annonce not in seen_ids:
-                new_ids.add(id_annonce)
-        seen_ids.update(new_ids)
-            # on ajoute les nouveaux id dans le json
-    with open("seen.json", "w") as f:
-        json.dump(list(seen_ids), f)
-    # Attendre 1 minute avant la prochaine requête
-    time.sleep(60)  
+                    requests.post(url_telegram, data=payload)
+                # si l'id n'est pas dans le json on le stock
+                if id_annonce not in seen_ids:
+                    new_ids.add(id_annonce)
+                seen_ids.update(new_ids)
+                # on ajoute les nouveaux id dans le json
+            with open("seen.json", "w") as f:
+                json.dump(list(seen_ids), f)
+        # Attendre 5 minutes avant la prochaine requête
+    time.sleep(300)  
 
 # gérer les doublons
